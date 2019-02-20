7e8c5c499d413925f45196d7f042e0f3c64d56de
Kevin Simms <ksimms2@cnm.edu>
2019-02-20 16:12:04 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/life/controller/Life.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.life.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+
+public class Life {
+
+  @FXML
+  private Button start;
+
+
+
+  @FXML
+  private ScrollPane viewScroller;
+}
